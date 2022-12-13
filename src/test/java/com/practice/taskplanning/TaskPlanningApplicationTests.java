3eeef479a5d1be89afdf910b95a0d3e817d06e37
Vadim Vedenin <vedeninvv@mail.ru>
2022-12-13 21:40:37 +0300

    Init project
    Add dependencies in pom.xml.
    Add settings in application.properties to connect to
    database and configure work with it.

---
+++ src/test/java/com/practice/taskplanning/TaskPlanningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.taskplanning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskPlanningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
