31d4f2f30da77606c08b0fecf4f404666fd7b947
Vadim Vedenin <vedeninvv@mail.ru>
2022-12-13 21:40:41 +0300

    Add swagger-documentation for taskPoint module

--- src/main/java/com/practice/taskplanning/controller/TaskPointController.java
+++ src/main/java/com/practice/taskplanning/controller/TaskPointController.java
@@ -4,6 +4,12 @@ import com.practice.taskplanning.dto.taskPoint.TaskPointGetDto;
 import com.practice.taskplanning.dto.taskPoint.TaskPointPatchDto;
 import com.practice.taskplanning.dto.taskPoint.TaskPointPostDto;
 import com.practice.taskplanning.service.TaskPointService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
@@ -27,6 +33,14 @@ public class TaskPointController {
         this.taskPointService = taskPointService;
     }
 
+    @Operation(summary = "Create taskPoint", security = @SecurityRequirement(name = "basicAuth"),
+            description = "Create taskPoint in connection with task")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "TaskPoint was created"),
+            @ApiResponse(responseCode = "400", description = "Invalid taskPoint's data", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Task not found", content = @Content),
+            @ApiResponse(responseCode = "403", description = "Don't have permission to create taskPoint", content = @Content)
+    })
     @PreAuthorize("hasAuthority('task_point:write')")
     @PostMapping("/{taskId}/task-points")
     public TaskPointGetDto createTaskPoint(@PathVariable Long taskId,
@@ -34,6 +48,13 @@ public class TaskPointController {
         return taskPointService.createTaskPoint(taskId, taskPointPostDto);
     }
 
+    @Operation(summary = "Update taskPoint", security = @SecurityRequirement(name = "basicAuth"))
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "TaskPoint was updated"),
+            @ApiResponse(responseCode = "404", description = "TaskPoint not found", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Invalid taskPoint's data", content = @Content),
+            @ApiResponse(responseCode = "403", description = "Don't have permission to update taskPoint", content = @Content)
+    })
     @PreAuthorize("hasAuthority('task_point:write')")
     @PatchMapping("/task-points/{taskPointId}")
     public TaskPointGetDto updateTaskPoint(@PathVariable Long taskPointId,
@@ -41,23 +62,47 @@ public class TaskPointController {
         return taskPointService.updateTaskPoint(taskPointId, taskPointPatchDto);
     }
 
+    @Operation(summary = "Get taskPoint by id", security = @SecurityRequirement(name = "basicAuth"))
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "TaskPoint successfully found by id"),
+            @ApiResponse(responseCode = "404", description = "TaskPoint not found", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Id isn't number", content = @Content),
+            @ApiResponse(responseCode = "403", description = "Don't have permission to get taskPoint", content = @Content)
+    })
     @PreAuthorize("hasAuthority('task_point:read')")
     @GetMapping("/task-points/{taskPointId}")
     public TaskPointGetDto getTaskPointById(@PathVariable Long taskPointId) {
         return taskPointService.getTaskPointById(taskPointId);
     }
 
+    @Operation(summary = "Get page of taskPoints", security = @SecurityRequirement(name = "basicAuth"),
+            description = "Get page of taskPoints with filters by task, completed status, matching search string with name" +
+                    " or description, start date and end date (year-month-day)")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Page of taskPoints"),
+            @ApiResponse(responseCode = "400", description = "Bad parameters type or format", content = @Content),
+            @ApiResponse(responseCode = "403", description = "Don't have permission to get taskPoints", content = @Content)
+    })
     @PreAuthorize("hasAuthority('task_point:read')")
     @GetMapping("/task-points")
     public Iterable<TaskPointGetDto> getAllTaskPoints(@RequestParam(required = false) Long taskId,
                                                       @RequestParam(required = false) Boolean completed,
                                                       @RequestParam(required = false) String searchStr,
-                                                      @DateTimeFormat(pattern = "yyyy-MM-dd") @RequestParam(required = false) Date createdDateBegin,
-                                                      @DateTimeFormat(pattern = "yyyy-MM-dd") @RequestParam(required = false) Date createdDateEnd,
+                                                      @Parameter(description = "Date format: 'yyyy-MM-dd'") @DateTimeFormat(pattern = "yyyy-MM-dd")
+                                                      @RequestParam(required = false) Date createdDateBegin,
+                                                      @Parameter(description = "Date format: 'yyyy-MM-dd'") @DateTimeFormat(pattern = "yyyy-MM-dd")
+                                                      @RequestParam(required = false) Date createdDateEnd,
                                                       @ParameterObject @PageableDefault(sort = "id", direction = Sort.Direction.ASC) Pageable pageable) {
         return taskPointService.getAllTaskPoints(taskId, completed, searchStr, createdDateBegin, createdDateEnd, pageable);
     }
 
+    @Operation(summary = "Delete taskPoint", security = @SecurityRequirement(name = "basicAuth"))
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "TaskPoint was deleted"),
+            @ApiResponse(responseCode = "404", description = "TaskPoint not found", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Id isn't number", content = @Content),
+            @ApiResponse(responseCode = "403", description = "Don't have permission to delete taskPoint", content = @Content)
+    })
     @PreAuthorize("hasAuthority('task_point:write')")
     @DeleteMapping("/task-points/{taskPointId}")
     public void deleteTaskPoint(@PathVariable Long taskPointId) {
